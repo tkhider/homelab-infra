3743821b7b5f28b37966dbf0c032ea2c4cf51f62
Tarek KHIDER <tarek@khider.fr>
2026-07-26 17:11:04 +0200

    dropping tarek.khider from dns records, project is on hold

--- terraform/cloudflare/dns_records.tf
+++ terraform/cloudflare/dns_records.tf
@@ -80,20 +80,6 @@ resource "cloudflare_dns_record" "torrent" {
   }
 }
 
-resource "cloudflare_dns_record" "tarek" {
-  content = "khider.fr"
-  name    = "tarek.khider.fr"
-  proxied = true
-  tags    = []
-  ttl     = 1
-  type    = "CNAME"
-  zone_id = "935f13b73c1d137163ea96517e12fcb6"
-  settings = {
-    flatten_cname = false
-  }
-}
-
-
 resource "cloudflare_dns_record" "www" {
   content = "khider.fr"
   name    = "www.khider.fr"
