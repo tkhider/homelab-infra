58393515d1b85d0b194fb4ed0f3e2d6e89319787
Tarek KHIDER <tarek@khider.fr>
2026-09-07 02:18:33 +0200

    terraforming my proxmox state

---
+++ terraform/proxmox/providers.tf
@@ -0,0 +1,30 @@
+variable "api_token" {
+  type = string
+  description = "Proxmox API Token"
+  sensitive = true
+}
+
+terraform {
+  required_version = "1.16"
+
+  cloud {
+
+    organization = "Tarek-Corp"
+
+    workspaces {
+      name = "proxmox"
+    }
+  }
+  required_providers {
+    proxmox = {
+      source  = "bpg/proxmox"
+      version = "0.112.0"
+    }
+  }
+}
+
+provider "proxmox" {
+  endpoint  = "https://elior.local.khider.fr"
+  api_token = var.api_token
+  insecure  = false
+}
